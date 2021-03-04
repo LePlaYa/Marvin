1d62f39e38f8ebbfa1edab1fd709a275ea222ef8
Jesse Scheumann <jesse.scheumann@gmx.de>
2021-03-04 15:51:28 +0100

    create temporary channel exclude to seperate file

--- Marvin.py
+++ Marvin.py
@@ -1,5 +1,6 @@
 import discord
 import w2g
+import temporary_channel
 
 class MyClient(discord.Client):
     created_channel = []
@@ -16,16 +17,7 @@ class MyClient(discord.Client):
             await message.channel.send('falsch')
 
     async def on_voice_state_update(self, member, before, after):
-        #print("{}:{}:{}".format(member, before, after))
-        if(after.channel):
-            if(after.channel.name == "create-room"):
-                self.created_channel.append(await after.channel.guild.create_voice_channel("{0.name}'s Channel".format(member), category=after.channel.category))
-                await member.move_to(self.created_channel[-1])
-
-        for channel in self.created_channel:
-            if(not len(channel.members)):
-                await channel.delete()
-                self.created_channel.remove(channel)
+        await temporary_channel.temporary_channel(self, member, before, after)
 
 
 client = MyClient()
---
+++ temporary_channel.py
@@ -0,0 +1,10 @@
+async def temporary_channel(self, member, before, after):
+    if(after.channel):
+        if(after.channel.name == "create-room"):
+            self.created_channel.append(await after.channel.guild.create_voice_channel("{0.name}'s Channel".format(member), category=after.channel.category))
+            await member.move_to(self.created_channel[-1])
+
+    for channel in self.created_channel:
+        if(not len(channel.members)):
+            await channel.delete()
+            self.created_channel.remove(channel)
