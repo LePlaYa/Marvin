a786c84d944bc0e08b4a929a14dbf440a2a779ca
Jesse Scheumann <jesse.scheumann@gmx.de>
2021-03-11 17:38:00 +0100

    delete wrong line in code by a merge conflict mistake by myself

--- temporary_channel.py
+++ temporary_channel.py
@@ -1,7 +1,6 @@
 async def temporary_channel(self, member, before, after):
     if(after.channel):
         if(after.channel.name == "join-to-create-temp-channels"):
-            self.created_channel.append(await after.channel.guild.create_voice_channel("{0.name}'s Channel".format(member), category=after.channel.category))
             self.created_channel.update({await after.channel.guild.create_voice_channel("{0.name}'s Channel".format(member), category=after.channel.category) : await after.channel.guild.create_text_channel("{0.name}'s Text Channel".format(member), category=after.channel.category)})
             await member.move_to(list(self.created_channel.keys())[-1])
 
